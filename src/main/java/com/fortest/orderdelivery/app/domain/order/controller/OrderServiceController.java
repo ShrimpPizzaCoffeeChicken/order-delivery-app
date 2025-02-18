5a53331069ac65ddb3bb25f9087893a53540a423
jeonjinwoo <hp5234@naver.com>
2025-02-18 09:50:02 +0900

    [feat] : ResponseEntity 반영

--- src/main/java/com/fortest/orderdelivery/app/domain/order/controller/OrderServiceController.java
+++ src/main/java/com/fortest/orderdelivery/app/domain/order/controller/OrderServiceController.java
@@ -5,6 +5,7 @@ import com.fortest.orderdelivery.app.domain.order.service.OrderService;
 import com.fortest.orderdelivery.app.global.dto.CommonDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,15 +24,17 @@ public class OrderServiceController {
     private final OrderService orderService;
 
     @PostMapping("/api/service/orders")
-    public CommonDto<?> saveOrder(@RequestBody OrderSaveRequestDto orderSaveRequestDto) {
+    public ResponseEntity<CommonDto<Map<String, String>>> saveOrder(@RequestBody OrderSaveRequestDto orderSaveRequestDto) {
         // TODO : TEMP : userId 를 UserDetail 에서 획득해야함
         String orderId = orderService.saveOrder(orderSaveRequestDto, 123L);
-        Map<String, String> body = Map.of("order-id", orderId);
+        Map<String, String> data = Map.of("order-id", orderId);
 
-        return CommonDto.builder()
-                .code(HttpStatus.OK.value())
-                .message("Success")
-                .data(body)
-                .build();
+        return ResponseEntity.ok(
+                CommonDto.<Map<String, String>> builder()
+                        .code(HttpStatus.OK.value())
+                        .message("Success")
+                        .data(data)
+                        .build()
+        );
     }
 }
