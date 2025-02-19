521531bf1a79b343ccad0536e776b9349bd3d478
jeonjinwoo <hp5234@naver.com>
2025-02-20 01:43:39 +0900

    [feat] : 배달 목록 조회 기능 추가

---
+++ src/main/java/com/fortest/orderdelivery/app/domain/delivery/dto/DeliveryGetListDto.java
@@ -0,0 +1,36 @@
+package com.fortest.orderdelivery.app.domain.delivery.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryGetListDto {
+
+    @JsonProperty("total-contents")
+    private Long totalContents;
+    private String search;
+    private Integer size;
+    @JsonProperty("current-page")
+    private Integer currentPage;
+    @JsonProperty("delivery-list")
+    private List<DeliveryDto> deliveryList;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DeliveryDto {
+        @JsonProperty("delivery-id")
+        private String deliveryId;
+        private String address;
+        private String status;
+    }
+}
--- src/main/java/com/fortest/orderdelivery/app/domain/delivery/mapper/DeliveryMapper.java
+++ src/main/java/com/fortest/orderdelivery/app/domain/delivery/mapper/DeliveryMapper.java
@@ -1,8 +1,12 @@
 package com.fortest.orderdelivery.app.domain.delivery.mapper;
 
+import com.fortest.orderdelivery.app.domain.delivery.dto.DeliveryGetListDto;
 import com.fortest.orderdelivery.app.domain.delivery.dto.DeliverySaveRequestDto;
 import com.fortest.orderdelivery.app.domain.delivery.dto.DeliverySaveResponseDto;
 import com.fortest.orderdelivery.app.domain.delivery.entity.Delivery;
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
 
 public class DeliveryMapper {
 
@@ -19,5 +23,25 @@ public class DeliveryMapper {
                 .deliveryId(delivery.getId())
                 .deliveryStatus(delivery.getStatus().name())
                 .build();
+    }
+
+    public static DeliveryGetListDto entityToGetListDto (Page<Delivery> page, String search) {
+        DeliveryGetListDto.DeliveryGetListDtoBuilder builder = DeliveryGetListDto.builder();
+        builder = builder
+                .search(search == null ? "" : search)
+                .totalContents(page.getTotalElements())
+                .size(page.getSize())
+                .currentPage(page.getNumber() + 1);
+        ArrayList<DeliveryGetListDto.DeliveryDto> deliveryDtos = new ArrayList<>();
+        for (Delivery delivery : page.getContent()) {
+            deliveryDtos.add(
+                    DeliveryGetListDto.DeliveryDto.builder()
+                            .deliveryId(delivery.getId())
+                            .address(delivery.getAddress())
+                            .status(delivery.getStatus().name())
+                            .build()
+            );
+        }
+        return builder.deliveryList(deliveryDtos).build();
     }
 }
