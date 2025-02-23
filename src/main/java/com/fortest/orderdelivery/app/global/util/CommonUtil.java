0f2d6531317e536daed5972605c7420d22673cd4
ggumi030 <tndus50843@gmail.com>
2025-02-23 15:26:22 +0900

    [refactor] : menu 양방향 매핑으로 수정

--- src/main/java/com/fortest/orderdelivery/app/global/util/CommonUtil.java
+++ src/main/java/com/fortest/orderdelivery/app/global/util/CommonUtil.java
@@ -1,11 +1,14 @@
 package com.fortest.orderdelivery.app.global.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fortest.orderdelivery.app.global.exception.BusinessLogicException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class CommonUtil {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static String LDTToString(LocalDateTime localDateTime) {
         if (localDateTime == null) {
@@ -21,5 +24,13 @@ public class CommonUtil {
     // 빈문자열 or null 이면 true
     public static boolean checkStringIsEmpty (String target) {
         return target == null || target.isEmpty();
+    }
+
+    public static <T> T convertJsonToDto(String jsonString, Class<T> classType) {
+        try {
+            return objectMapper.readValue(jsonString, classType);
+        } catch (Exception e) {
+            throw new BusinessLogicException("Json 변환 실패");
+        }
     }
 }
