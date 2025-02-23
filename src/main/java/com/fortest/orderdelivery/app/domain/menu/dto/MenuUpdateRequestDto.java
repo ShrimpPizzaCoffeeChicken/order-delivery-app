d43c381c0a6da36325baa117607234a0d515a552
ggumi030 <tndus50843@gmail.com>
2025-02-23 09:33:35 +0900

    [refactor] : dto validation 추가

--- src/main/java/com/fortest/orderdelivery/app/domain/menu/dto/MenuUpdateRequestDto.java
+++ src/main/java/com/fortest/orderdelivery/app/domain/menu/dto/MenuUpdateRequestDto.java
@@ -1,6 +1,9 @@
 package com.fortest.orderdelivery.app.domain.menu.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,11 +14,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MenuUpdateRequestDto {
-    //    @NotBlank(message = "[name:blank]")
+    @Size(min = 1, max = 100, message = "메뉴 이름은 필수 입력값입니다.")
     private String name;
+
     private String description;
-    //    @NotBlank(message = "[price:blank]")
+
+    @Positive(message = "price 는 양의 정수만 입력 가능합니다.")
     private Integer price;
+
+    @Pattern(regexp = "ONSALE|SOLDOUT|HIDING", message = "menu-expose-status는 ONSALE, SOLDOUT, HIDING 만 허용합니다.")
     @JsonProperty("expose-status")
     private String exposeStatus;
 }
